5594dec285f7104b80073e10624964ce93b77617
Lucas Templeton <templetl99@gmail.com>
2021-11-04 11:26:47 -0500

    commit tree f466808689470077656c93720c4aa3272d6428b7

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
 // Code your solution here
+// const totalBatteries = batteryBatches.reduce(reducer);
+
+const totalBatteries = batteryBatches.reduce((previousValue, currentValue) => previousValue + currentValue);
